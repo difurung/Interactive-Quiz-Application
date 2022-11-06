41e81ca9dac4899c59b0b0e7c3790ec9efcb9d58
denart ifurung <dfurung@hotmail.com>
2022-11-06 11:18:00 -0500

    worked on rendering questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,71 +1,71 @@
- var currentQuestion = 0
- var score = 0
+//  var currentQuestion = 0
+//  var score = 0
  
- 
- var questions = [
-  {
-    title: 'Biology is the study of:',
-    choices: ['Life', 'Tectonics', 'Chemicals', 'Nuclear Reactions'],
-    answer: 'Life',
-  },
+//   var questions = [
+//   {
+//     title: 'Biology is the study of:',
+//     choices: ['Life', 'Tectonics', 'Chemicals', 'Nuclear Reactions'],
+//     answer: 'Life',
+//   },
 
 
 
-  {
-    title: 'DNA is found in the:',
-    choices: ['Golgi apparatus', 'Mitochondria', 'Nucleus', '2 of the above'],
-    answer: '2 of the above',
-  },
+//   {
+//     title: 'DNA is found in the:',
+//     choices: ['Golgi apparatus', 'Mitochondria', 'Nucleus', '2 of the above'],
+//     answer: '2 of the above',
+//   },
 
 
 
-  {
-    title: 'What is the gravitational acceleration of earth?',
-    choices: ['9.8 m/s2', '3.72 m/s2', '1.62 m/s2', '8.87 m/s2'],
-    answer: '9.8 m/s2',
-  },
+//   {
+//     title: 'What is the gravitational acceleration of earth?',
+//     choices: ['9.8 m/s2', '3.72 m/s2', '1.62 m/s2', '8.87 m/s2'],
+//     answer: '9.8 m/s2',
+//   },
 
 
 
-  {
-    title: 'The term cardiac refers to the:',
-    choices: ['Heart', 'Kidneys', 'Liver', 'pancreas'],
-    answer: 'Heart',
-  },
+//   {
+//     title: 'The term cardiac refers to the:',
+//     choices: ['Heart', 'Kidneys', 'Liver', 'pancreas'],
+//     answer: 'Heart',
+//   },
 
 
 
-  {
-    title: 'What is the largest organ in the human body?',
-    choices: ['Heart', 'Lungs', 'Skin', 'Intestinal tract'],
-    answer: 'Skin',
-  },
+//   {
+//     title: 'What is the largest organ in the human body?',
+//     choices: ['Heart', 'Lungs', 'Skin', 'Intestinal tract'],
+//     answer: 'Skin',
+//   },
 
 
 
-  {
-    title: 'The surface tension of water is representative of which type of chemical bond?',
-    choices: ['Ionic', 'Hydrogen', 'Covalent', 'Weak nuclear'],
-    answer: 'Hydrogen',
-  },
+//   {
+//     title: 'The surface tension of water is representative of which type of chemical bond?',
+//     choices: ['Ionic', 'Hydrogen', 'Covalent', 'Weak nuclear'],
+//     answer: 'Hydrogen',
+//   },
 
 
 
-  {
-    title: 'Chronic high blood glucose in diabetics will adversely affect the:',
-    choices: ['Eyes', 'Kidneys', 'Heart', 'All of the above'],
-    answer: 'All of the above',
-  },
+//   {
+//     title: 'Chronic high blood glucose in diabetics will adversely affect the:',
+//     choices: ['Eyes', 'Kidneys', 'Heart', 'All of the above'],
+//     answer: 'All of the above',
+//   },
 
 
 
-  {
-    title: 'Who is considered the father of modern medicine?:',
-    choices: ['Dr. Jonas Salk', 'Dr. Edward Jenner', 'Hippocrates', 'Dr. Acula'],
-    answer: 'Hippocrates',
-  },
+//   {
+//     title: 'Who is considered the father of modern medicine?:',
+//     choices: ['Dr. Jonas Salk', 'Dr. Edward Jenner', 'Hippocrates', 'Dr. Acula'],
+//     answer: 'Hippocrates',
+//   },
 
-];
+// ]
+
 
 // will learn a library called inquirer (npm package)
     
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,102 @@
 
 //Create a timer started by Get started button
 //var start = document.querySelector("start");
+var currentQuestion = 0
 var score = 0
 
 
 var startCon = document.getElementById("#startContainer");
 var startEl = document.querySelector(".startBtn");
-var questionCardEl = document.querySelector("#questionCard");
+var questionCardEl = document.querySelector(".questionCard");
+var questionTitle = document.getElementById("#question")
+var answerEl = document.getElementById("#answer-choices")
+var qindex = 0
 var initialsEl = document.querySelector("#initials");
 var scoreEl = document.querySelector("#score");
 
 var timeEl = document.querySelector(".time");
-
 var secondsLeft = 75;
 
 
 
+var currentQuestion = 0
+var score = 0
+
+
+ 
+  var questions = [
+  {
+    title: 'Biology is the study of:',
+    choices: ['Life', 'Tectonics', 'Chemicals', 'Nuclear Reactions'],
+    answer: 'Life',
+  },
+
+
+
+  {
+    title: 'DNA is found in the:',
+    choices: ['Golgi apparatus', 'Mitochondria', 'Nucleus', '2 of the above'],
+    answer: '2 of the above',
+  },
+
+
+
+  {
+    title: 'What is the gravitational acceleration of earth?',
+    choices: ['9.8 m/s2', '3.72 m/s2', '1.62 m/s2', '8.87 m/s2'],
+    answer: '9.8 m/s2',
+  },
+
+
+
+  {
+    title: 'The term cardiac refers to the:',
+    choices: ['Heart', 'Kidneys', 'Liver', 'pancreas'],
+    answer: 'Heart',
+  },
+
+
+
+  {
+    title: 'What is the largest organ in the human body?',
+    choices: ['Heart', 'Lungs', 'Skin', 'Intestinal tract'],
+    answer: 'Skin',
+  },
+
+
+
+  {
+    title: 'The surface tension of water is representative of which type of chemical bond?',
+    choices: ['Ionic', 'Hydrogen', 'Covalent', 'Weak nuclear'],
+    answer: 'Hydrogen',
+  },
+
+
+
+  {
+    title: 'Chronic high blood glucose in diabetics will adversely affect the:',
+    choices: ['Eyes', 'Kidneys', 'Heart', 'All of the above'],
+    answer: 'All of the above',
+  },
+
+
+
+  {
+    title: 'Who is considered the father of modern medicine?:',
+    choices: ['Dr. Jonas Salk', 'Dr. Edward Jenner', 'Hippocrates', 'Dr. Acula'],
+    answer: 'Hippocrates',
+  },
+
+];
+
+
+// will learn a library called inquirer (npm package)
+    
+
+
+
 // hide the page
-questionCardEl.style.display = "none"
+//questionCardEl.style.display = "none"
 initialsEl.style.display = "none"
 scoreEl.style.display = "none"
 
@@ -33,11 +112,11 @@ var letsBegin = function () {
   
   startContainer.style.display = "none"
   
-  questionCardEl.style.display = "flex"
+  questionCard.style.display = "flex"
   
   //var textContainer = document.querySelector("text-container")
   timer()
-  //listQuestion() 
+  listQuestion() 
 };
 
 var timer = function setTime() {  
@@ -52,74 +131,44 @@ var timer = function setTime() {
       clearInterval(timerInterval)
       
       
-      sendMessage();
+      //sendMessage();
     }
     
   }, (1000));
 };
 
+
+
+//var listQuestions = function giveQuestion
+
+
+
+var questionTitle = document.querySelector("#question");
+
+var listQuestion = function () {
+  
+  // Add question and respective answer choices text
+ questionTitle.textContent = questions[qindex].title;
+ 
+  
+  // Create element for each answer choice and append to answersEl div
+  for (var i = 0; i < 4; i++) {
+    var playerChoice = document.createElement("p");
+    playerChoice.textContent = questions[qindex].choices[i];
+    playerChoice.className = "answer-choice";
+    answerEl.appendChild(playerChoice);
+      console.log(playerChoice)
+  }
+  
+  // Event listener for clicking on answer choices
+  //answersEl.addEventListener("click", checkAnswer);
+};
+
+// var listQuestions = function giveQuestion
+
 startEl.addEventListener("click", letsBegin)
 
 
-// var listQuestions = function giveQuestion
-
-// var questionTitle = questionPageEl.querySelector("#question");
-
-// var listQuestion = function () {
-
-//   questionTitle.textContent = questions[qIndex].question;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var listQuestions = function giveQuestion
-
-
-
-
-// (startEl.addEventListener("click", letsBegin()));
-
-
-// const lastQuestion = questions.length - 1;
-
-// let runningQuestion = 0;
-
-// function renderQuestion(){
-
-//     let q = questions[runningQuestion];
-
-
-
-//     question.innerHTML = "<h2>"+ q.question +"</h2>";
-
-//         choiceA.innerHTML = q.choiceA;
-
-//     choiceB.innerHTML = q.choiceB;
-
-//     choiceC.innerHTML = q.choiceC;
-
-// }
-
-// function renderProgress(){
-
-//     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-
-//         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-
-//     }
-
-// }
 
 
 
